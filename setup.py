934da54626443ef91e4f9a6d11b5b4f6abd144bc
Itay Ben Dan <itaybd@gmail.com>
2024-01-31 13:20:41 +0200

    creating setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TreeModelVis',
+    version='0.1.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A toolkit for visualizing and customizing tree-based models.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/bdi2357/TreeModelVis',
+    packages=find_packages(),
+    install_requires=open('requirements.txt').read().splitlines(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
